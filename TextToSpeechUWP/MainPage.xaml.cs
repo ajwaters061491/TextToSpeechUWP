ca933442c0c7fb00d06eaff8801078d91a5c2bc4
Alvin Waters <alvin.waters0614@gmail.com>
2019-04-22 02:09:39 -0400

    Add project files.

---
+++ TextToSpeechUWP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Media.SpeechSynthesis; //to read
+
+
+namespace TextToSpeechUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        SpeechSynthesizer speechSynthesizer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            speechSynthesizer = new SpeechSynthesizer();
+        }
+
+        private void BtnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Talk(txtTextToSpeech.Text); //this is the text box's location
+        }
+
+        private async void Talk(string message) //method to read the message passed in
+        {
+            var stream = await speechSynthesizer.SynthesizeTextToStreamAsync(message);
+            media.SetSource(stream, stream.ContentType);
+            media.Play();
+        }
+    }
+}
